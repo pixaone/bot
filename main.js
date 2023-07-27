3bb827afe7fd761a30a9a999e728c0055a3660a8
pixaone <128310316+pixaone@users.noreply.github.com>
2023-07-27 14:18:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const discord = require('discord.js');
+
+const clinet = new Discord.Client();
+
+
+
+
+clinet.once('ready', () =>{
+    console.log('ƊŁャ System is Online');
+});
+
+
+clinet.login('MTEzMDYzMjIxMzYxMjk5ODcwNg.GwUdEi.xSRnFrT63CqahiZ3SxDnTCcOJbk4eGW7x8_KXA');
